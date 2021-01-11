de7840e60984fd0b5c879f331081ff34bc0cb806
JesseRedford <Jesse.k.redford@gmail.com>
2021-01-11 16:53:56 -0500

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -5,5 +5,5 @@ st.set_page_config(layout="wide")
 st.title('Surface Measurment App')
 st.subheader('Author: Jesse Redford')
 
-uploaded_file = st.file_uploader("Upload a surface image", type=([".png",".jpg"]))
+#uploaded_file = st.file_uploader("Upload a surface image", type=([".png",".jpg"]))
 
