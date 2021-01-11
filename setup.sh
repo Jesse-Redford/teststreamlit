0711634dff8528fd62b24bf06cc1b3f36a77e2d9
JesseRedford <Jesse.k.redford@gmail.com>
2021-01-11 15:59:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"jesse.k.redford@gmail.com\"\n\
+echo "[general]
+email = \"jesse.k.redford@gmail.com\"
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
