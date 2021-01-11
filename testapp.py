1391ad3a13a2af6ca88274b5072d340735e51208
JesseRedford <Jesse.k.redford@gmail.com>
2021-01-11 15:37:44 -0500

    Add files via upload

---
+++ testapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+st.title('Surface Measurment App')
+st.subheader('Author: Jesse Redford')
+
+uploaded_file = st.file_uploader("Upload a surface image", type=([".png",".jpg"]))
+
